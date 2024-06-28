64d02a5227899e785665b8ddbea62fae6af60c7c
Victor <ban.victor728@gmail.com>
2024-06-28 13:41:10 -0400

    workshop done. making url to link it

---
+++ index.js
@@ -0,0 +1,20 @@
+/*Pseudo code
+
+Need 4 variables.
+
+One for the string. so const variableName = `string`;
+One for each of the numbers. const number = someOperations
+
+Need to put those variables inside the pop up box so they display when accessed.
+
+*/
+
+const codeOne = 6 + 5 * 3 - 11 ;
+const codeTwo = 100 / 5 - 13 + 33;
+const codeThree = (20 % 6) + 37;
+
+
+let string = `You have received this message because you have been chosen to open an important vault.
+ Here is the secret combination: ${codeOne}-${codeTwo}-${codeThree}`;
+
+ alert(string);
